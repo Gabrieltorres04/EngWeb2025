3667ac59ed36ef46ada16505dd68533008459994
Gabriel Torres <gabrieltorres5az@gmail.com>
2025-02-16 23:56:31 +0000

    TPC1

---
+++ TPC1/server.js
@@ -0,0 +1,78 @@
+const http = require('http');
+const meta = require('./aux');
+const axios = require('axios');
+
+
+
+http.createServer((req,res,err)=>{
+    console.log("METHOD: " + req.method)
+    console.log("URL:" + req.url)
+    
+    switch(req.method) {
+        case 'GET' :         
+            switch(req.url) {
+                case "/reparacoes":           
+                    axios.get("http://localhost:3001/reparacoes")
+                    .then(result => {
+                        res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+                        res.write('<h1>Lista de Reparações</h1>');
+                        res.write('<table border="1"><tr><th>Data</th><th>NIF</th><th>Nome</th><th>Marca</th><th>Modelo</th><th>Número de Intervenções</th></tr>');
+                        response.data.forEach(reparacao => {
+                            res.write(`<tr>
+                                <td>${reparacao.data}</td>
+                                <td>${reparacao.nif}</td>
+                                <td>${reparacao.nome}</td>
+                                <td>${reparacao.viatura.marca}</td>
+                                <td>${reparacao.viatura.modelo}</td>
+                                <td>${reparacao.nr_intervencoes}</td>
+                            </tr>`);
+                        });
+                        res.write('</table>');
+                        res.end();
+                    }) 
+                    .catch (err => {
+                        res.writeHead(500, {'Content-Type' : 'text/html ; charset=utf-8'})
+                        console.log(err)
+                        res.end()
+                    })
+
+                    return;
+                    
+                case "/marcas":
+                   
+                    const response = axios.get("http://localhost:3001/reparacoes")
+                    .then(result => {
+                    const marcas = new Set();
+                    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+                    res.write('<h1>Lista de Marcas</h1>');
+                    res.write('<ul>');
+                    response.data.forEach(reparacao => {
+                        if(!marcas.has(reparacao.viatura.marca)){
+                            marcas.add(reparacao.viatura.marca);
+                            res.write(`<li>${reparacao.viatura.marca}</li>`);
+                        }
+                    });
+                    res.write('</ul>');
+                    res.end();
+                    }) 
+                    .catch (err => {
+                    res.writeHead(500, {'Content-Type' : 'text/html ; charset=utf-8'})
+                    console.log(err)
+                    res.end()
+                    })
+                    return;
+
+
+                default:
+                    res.writeHead(404, {'Content-Type': 'text/html;charset=utf-8'});
+                    res.end();
+                    return;
+            }
+        default : 
+            res.statusCode(405)
+            return;
+    }
+    res.end()
+}).listen(1234)
+
+console.log('Servidor á escuta na porta 1234')
