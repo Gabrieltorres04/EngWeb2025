cb43fb76035cd59804a6f0dd1b61ec3478f9e264
Gabriel Torres <gabrieltorres5az@gmail.com>
2025-04-09 22:53:42 +0100

    almost done

---
+++ TPC5/apiAlunos/routes/alunos.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var Aluno = require('../controllers/alunos')
+
+/* GET alunos listing */
+router.get('/', function(req, res, next) {
+    Aluno.list()
+        .then(data => res.jsonp(data))
+        .catch(erro => res.jsonp(erro))
+});
+
+/* GET aluno by id */
+router.get('/:id', function(req, res, next) {
+    Aluno.findById(req.params.id)
+        .then(data => res.jsonp(data))
+        .catch(erro => res.jsonp(erro))
+});
+
+/* POST alunos*/
+router.post('/', function(req, res, next) {
+    Aluno.insert(req.body)
+        .then(data => res.jsonp(data))
+        .catch(erro => res.jsonp(erro))
+});
+
+/* PUT aluno by id */
+router.put('/:id', function(req, res, next) {
+    Aluno.update(req.params.id, req.body)
+        .then(data => res.jsonp(data))
+        .catch(erro => res.jsonp(erro))
+});
+
+/* DELETE aluno by id */
+router.delete('/:id', function(req, res, next) {
+    Aluno.delete(req.params.id)
+        .then(data => res.jsonp(data))
+        .catch(erro => res.jsonp(erro))
+});
+
+/* PUT aluno by id */
+router.put('/:id/tpc/:idTpc', function(req, res, next) {
+    Aluno.inverteTpc(req.params.id, req.params.idTpc)
+        .then(data => res.jsonp(data))
+        .catch(erro => res.jsonp(erro))
+});
+
+
+module.exports = router;
---
+++ TPC5/frontAlunos/routes/alunos.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios')
+
+const API_URL = 'http://localhost:3000/alunos';
+
+// List all students
+router.get('/', function(req, res, next) {
+  axios.get(API_URL)
+    .then(response => {
+      res.render('alunos', {lalunos: response.data, title: 'Lista de Alunos'});
+    })
+    .catch(error => {
+      res.render('error', {error: error});
+    });
+});
+
+// Show student details
+router.get('/:id', function(req, res, next) {
+  axios.get(`${API_URL}/${req.params.id}`)
+    .then(response => {
+      res.render('alunoDetails', {aluno: response.data, title: 'Detalhes do Aluno'});
+    })
+    .catch(error => {
+      res.render('error', {error: error});
+    });
+});
+
+// Form to add a new student
+router.get('/registo/novo', function(req, res, next) {
+  res.render('newAlunoForm', {title: 'Novo Aluno'});
+});
+
+// Form to edit a student
+router.get('/edit/:id', function(req, res, next) {
+  axios.get(`${API_URL}/${req.params.id}`)
+    .then(response => {
+      res.render('editAlunoForm', {title: 'Editar Aluno', aluno: response.data});
+    })
+    .catch(error => {
+      res.render('error', {error: error});
+    });
+});
+
+// Save new student
+router.post('/registo', function(req, res, next) {
+  axios.post(API_URL, req.body)
+    .then(() => {
+      res.redirect('/alunos');
+    })
+    .catch(error => {
+      res.render('error', {error: error});
+    });
+});
+
+// Update student
+router.post('/edit/:id', function(req, res, next) {
+  axios.put(`${API_URL}/${req.params.id}`, req.body)
+    .then(() => {
+      res.redirect('/alunos');
+    })
+    .catch(error => {
+      res.render('error', {error: error});
+    });
+});
+
+// Delete student
+router.get('/delete/:id', function(req, res, next) {
+  axios.delete(`${API_URL}/${req.params.id}`)
+    .then(() => {
+      res.redirect('/alunos');
+    })
+    .catch(error => {
+      res.render('error', {error: error});
+    });
+});
+
+// Toggle TPC status
+router.get('/tpc/:id/:idTpc', function(req, res, next) {
+  axios.put(`${API_URL}/${req.params.id}/tpc/${req.params.idTpc}`)
+    .then(() => {
+      res.redirect('/alunos');
+    })
+    .catch(error => {
+      res.render('error', {error: error});
+    });
+});
+
+
+
+
+module.exports = router;
